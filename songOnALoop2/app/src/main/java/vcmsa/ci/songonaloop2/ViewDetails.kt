ab8690ab25fa7158cf383f5b411320f7c1316b04
ST10479849 <ST10479849@imconnect.edu.za>
2025-06-19 11:11:20 +0200

    initial commit
    new app

---
+++ songOnALoop2/app/src/main/java/vcmsa/ci/songonaloop2/ViewDetails.kt
@@ -0,0 +1,24 @@
+package vcmsa.ci.songonaloop2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewDetails : AppCompatActivity() {
+
+    private lateinit var songTitle: Array<String>
+    private lateinit var aristName: Array<String>
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
