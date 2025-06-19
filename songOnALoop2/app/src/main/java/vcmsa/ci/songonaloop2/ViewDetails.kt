3313355939f350cbed988aefb7d0a25451f064f0
ST10479849 <ST10479849@imconnect.edu.za>
2025-06-19 11:56:25 +0200

    Second commit
    second screen done , updating code

--- songOnALoop2/app/src/main/java/vcmsa/ci/songonaloop2/ViewDetails.kt
+++ songOnALoop2/app/src/main/java/vcmsa/ci/songonaloop2/ViewDetails.kt
@@ -10,11 +10,19 @@ class ViewDetails : AppCompatActivity() {
 
     private lateinit var songTitle: Array<String>
     private lateinit var aristName: Array<String>
-    
+    private lateinit var comments: Array<String>
+    private lateinit var ratings: Array<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_details)
+
+
+
+
+
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
