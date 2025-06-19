0aefa30dd76e39f06c74c2e835b17b92ef01ba9d
ST10479849 <ST10479849@imconnect.edu.za>
2025-06-19 08:49:51 +0200

    initial commit
    user interface for main scree completed
    arrays completed

---
+++ songOnALoop/app/src/main/java/za/ac/iie/songonaloop/MainActivity.kt
@@ -0,0 +1,49 @@
+package za.ac.iie.songonaloop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    // Error Handling
+
+
+
+
+
+     val songTitle = arrayOf("Ibhaynoi","Wish you were here","lomusa Ongaka","Somandla","Vuka")
+     val artistName = arrayOf("Blaq Diamond","Black Coffe","Spirit Of Praise","Mandisi","Osar")
+     val rating = intArrayOf(3,4,2,3,1)
+     val comments = arrayOf("Pop","House","Gospel","Jazz","Amapiano")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Sending the array information to second screen
+        val intent = Intent(this,ViewDetails::class.java)
+        intent.putExtra("songTitle",songTitle)
+        intent.putExtra("artistName",artistName)
+        intent.putExtra("rating",rating)
+        intent.putExtra("comments",comments)
+        
+        startActivity(intent)
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
